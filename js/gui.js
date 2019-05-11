bd32726cf005edaaabdb054f22c6410160fbac7b
Lauren von Berg <lmberg@princeton.edu>
2019-05-11 17:06:21 -0400

    snow

--- js/gui.js
+++ js/gui.js
@@ -3,9 +3,19 @@ var guiEnabled = true;
 if (guiEnabled) {
   // GUI properties
   guiControls = new (function() {
-    
+    this.snow = snow;
   })();
 
   // GUI elements
   gui = new dat.GUI();
+
+  let interactionControls = gui.addFolder("Weather");
+
+  interactionControls
+    .add(guiControls, "snow")
+    .name("Snow")
+    .onChange(function(value) {
+      snow = value;
+      addSnow();
+    });
 }
--- js/render.js
+++ js/render.js
@@ -4,35 +4,50 @@ var container;
 var stats;
 var controls;
 var camera, scene, renderer;
+var loader;
 var time;
 
 var groundMaterial;
 var light;
 
 // Objects in the scene
-var sphere;
+var sun;
 var box;
 var boundingBox;
 
 var gui;
 var guiControls;
 
+var snow = false;
 
 // Property of the ground floor in the scene
 var GROUND_Y = -249;
-
-// Properties of the sphere in the scene
-var sphereSize = 125;
-var spherePosition = new THREE.Vector3(0, -250 + sphereSize, 0);
-var prevSpherePosition = new THREE.Vector3(0, -250 + sphereSize, 0);
-
-var cylinderSize = 100;
-var cylinderHeight = 300;
-var cylinderBottom = new THREE.Vector3(0, -250, 0);
-var cylinderTop = new THREE.Vector3(0, -250 + cylinderHeight, 0);
+var sunPosition = new THREE.Vector3(100, 400, 1500);
 
 init();
 animate();
+
+function generateParticles(n, size, texture) {
+  // Add n random points to geometry
+  var geometry = new THREE.Geometry();
+  var range = 2000;
+  for (i = 0; i < n; i++) {
+    var x = Math.random() * 2 * range - range;
+    var y = Math.random() * 2 * range - range;
+    var z = Math.random() * 2 * range - range;
+    var point = new THREE.Vector3(x, y, z);
+    geometry.vertices.push(point);
+  }
+  // Map points to texture
+  var material = new THREE.PointsMaterial({
+    size: size,
+    map: texture,
+    blending: THREE.AdditiveBlending,
+    transparent: true
+  });
+
+  return new THREE.Points(geometry, material);
+}
 
 function init() {
 
@@ -55,7 +70,7 @@ function init() {
   renderer.setClearColor(scene.fog.color);
 
   // Loader for textures
-  var loader = new THREE.TextureLoader();
+  loader = new THREE.TextureLoader();
 
   container.appendChild(renderer.domElement);
   renderer.gammaInput = true;
@@ -72,8 +87,7 @@ function init() {
   // lights (fourth thing you need is lights)
   scene.add(new THREE.AmbientLight(0x666666));
   light = new THREE.DirectionalLight(0xdfebff, 1.75);
-  light.position.set(50, 200, 100);
-  light.position.multiplyScalar(1.3);
+  light.position.copy(sunPosition);
   light.castShadow = true;
   light.shadow.mapSize.width = 1024;
   light.shadow.mapSize.height = 1024;
@@ -100,12 +114,14 @@ function init() {
   displacementTexture.repeat.set( 25, 25 );
   displacementTexture.anisotropy = 16;
 
+  //scene.background = new THREE.Color( 'skyblue' );
+
   // create a buffer with color data
-  var width = 50; height = 50;
+  var width = 30; height = 30;
   var size = width * height;
   var data = new Uint8Array( 3 * size );
 
-  for ( var i = 0; i < size; i ++ ) {
+  for (var i = 0; i < size; i ++) {
   	var stride = i * 3;
   	data[ stride ] = Math.random() * 255;
   	data[ stride + 1 ] = Math.random() * 255;
@@ -121,22 +137,35 @@ function init() {
     specular: 0x404761, //0x3c3c3c//,
     map: groundTexture,
     displacementMap: texture,
-    displacementScale: 500
+    displacementScale: 50
   });
 
   console.log(displacementTexture);
 
   // ground mesh
-  let meshGeometry =new THREE.PlaneBufferGeometry(20000, 20000, 100, 100);
+  let meshGeometry =new THREE.PlaneBufferGeometry(20000, 500, 100, 100);
   let mesh = new THREE.Mesh(meshGeometry, groundMaterial);
   mesh.position.y = GROUND_Y - 1;
   mesh.rotation.x = -Math.PI / 2;
   mesh.receiveShadow = true;
   scene.add(mesh); // add ground to scene
-  console.log(meshGeometry);
-  console.log(mesh.displacementMap);
+
+  let sphereGeo = new THREE.SphereGeometry(5, 20, 20);
+  // sphere material
+  sphereMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 1,
+  });
 
 
+  // sphere mesh
+  sun = new THREE.Mesh(sphereGeo, sphereMaterial);
+  sun.castShadow = true;
+  sun.receiveShadow = false;
+  sun.position.copy(sunPosition);
+  scene.add(sun); // add sphere to scene
 
   // create a box mesh
   let boxGeo = new THREE.BoxGeometry(250, 100, 250);
@@ -161,6 +190,13 @@ function init() {
   window.addEventListener("resize", onWindowResize, false);
 }
 
+function addSnow() {
+  var snowTexture = loader.load( "textures/snowflake.png" );
+  var snowObject = generateParticles(20000, 20, snowTexture);
+  snowObject.rotation.x = Math.random() * 6;
+  scene.add(snowObject);
+}
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -172,8 +208,6 @@ function animate() {
 
   time = Date.now();
 
-  //light.position.add(new THREE.Vector3(1, 1, 1));
-
   render(); // update position of cloth, compute normals, rotate camera, render the scene
   stats.update();
   controls.update();
@@ -183,6 +217,17 @@ function animate() {
 function render() {
   let timer = Date.now() * 0.0002;
 
+  //console.log(light.position);
+  //let e = new THREE.Euler(-0.01, 0, 0, 'XYZ');
+  //light.position.applyEuler(e);
+  //sun.position.applyEuler(e);
+
   camera.lookAt(scene.position);
+
+  for (i = 0; i < scene.children.length; i++) {
+    if (scene.children[i] instanceof THREE.Points) {
+      scene.children[i].rotation.y += 0.01;
+    }
+  }
   renderer.render(scene, camera); // render the scene
 }
