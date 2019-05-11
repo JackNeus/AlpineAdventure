598f1c8a8a3b91bfe30a7090ac2c0025f301e8bb
Lauren von Berg <lmberg@princeton.edu>
2019-05-11 13:12:11 -0400

    ground plane

---
+++ js/render.js
@@ -0,0 +1,165 @@
+if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+var container;
+var stats;
+var controls;
+var camera, scene, renderer;
+var time;
+
+var clothObject;
+var clothGeometry;
+
+var snowflakeObject;
+var snowflakeGeometry;
+var snowflakeMaterial;
+
+var groundMaterial;
+
+// Objects in the scene
+var sphere;
+var box;
+var boundingBox;
+var cylinder;
+
+var gui;
+var guiControls;
+
+var poleMaterial, clothMaterial, sphereMaterial;
+
+// Property of the ground floor in the scene
+var GROUND_Y = -249;
+
+// Properties of the sphere in the scene
+var sphereSize = 125;
+var spherePosition = new THREE.Vector3(0, -250 + sphereSize, 0);
+var prevSpherePosition = new THREE.Vector3(0, -250 + sphereSize, 0);
+
+var cylinderSize = 100;
+var cylinderHeight = 300;
+var cylinderBottom = new THREE.Vector3(0, -250, 0);
+var cylinderTop = new THREE.Vector3(0, -250 + cylinderHeight, 0);
+
+init();
+animate();
+
+function init() {
+
+  container = document.createElement("div");
+  document.body.appendChild(container);
+
+  // scene (First thing you need to do is set up a scene)
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
+
+  // camera (Second thing you need to do is set up the camera)
+  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.y = 450;
+  camera.position.z = 1500;
+  scene.add(camera);
+
+  // renderer (Third thing you need is a renderer)
+  renderer = new THREE.WebGLRenderer({ antialias: true, devicePixelRatio: 1 });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(scene.fog.color);
+
+  container.appendChild(renderer.domElement);
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
+  renderer.shadowMap.enabled = true;
+
+  // This gives us stats on how well the simulation is running
+  stats = new Stats();
+  container.appendChild(stats.domElement);
+
+  // mouse controls
+  controls = new THREE.TrackballControls(camera, renderer.domElement);
+
+  // lights (fourth thing you need is lights)
+  let light, materials;
+  scene.add(new THREE.AmbientLight(0x666666));
+  light = new THREE.DirectionalLight(0xdfebff, 1.75);
+  light.position.set(50, 200, 100);
+  light.position.multiplyScalar(1.3);
+  light.castShadow = true;
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+
+  let d = 300;
+  light.shadow.camera.left = -d;
+  light.shadow.camera.right = d;
+  light.shadow.camera.top = d;
+  light.shadow.camera.bottom = -d;
+  light.shadow.camera.far = 1000;
+
+  scene.add(light);
+
+  // ground
+
+  /*
+  // needed for ground texture
+  var groundTexture = loader.load( "textures/terrain/grasslight-big.jpg" );
+  groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+  groundTexture.repeat.set( 25, 25 );
+  groundTexture.anisotropy = 16;
+  */
+
+  // ground material
+  groundMaterial = new THREE.MeshPhongMaterial({
+    color: 0x404761, //0x3c3c3c,
+    specular: 0x404761, //0x3c3c3c//,
+    //map: groundTexture
+  });
+
+  // ground mesh
+  let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(20000, 20000), groundMaterial);
+  mesh.position.y = GROUND_Y - 1;
+  mesh.rotation.x = -Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh); // add ground to scene
+
+  // create a box mesh
+  let boxGeo = new THREE.BoxGeometry(250, 100, 250);
+  boxMaterial = new THREE.MeshPhongMaterial({
+    color: 0xaaaaaa,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 0.01,
+  });
+  box = new THREE.Mesh(boxGeo, boxMaterial);
+  box.position.x = 0;
+  box.position.y = 0;
+  box.position.z = 0;
+  box.receiveShadow = true;
+  box.castShadow = true;
+  scene.add(box);
+
+  boxGeo.computeBoundingBox();
+  boundingBox = box.geometry.boundingBox.clone();
+
+  // event listeners
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  time = Date.now();
+
+  render(); // update position of cloth, compute normals, rotate camera, render the scene
+  stats.update();
+  controls.update();
+}
+
+// the rendering happens here
+function render() {
+  let timer = Date.now() * 0.0002;
+
+  camera.lookAt(scene.position);
+  renderer.render(scene, camera); // render the scene
+}
